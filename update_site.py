853d0a3118967be6f4b649483c028dda00600f6d
Kshitij Dahal <kdahal3@asu.edu>
2025-11-11 13:13:56 -0700

    site update

--- update_site.py
+++ update_site.py
@@ -349,7 +349,8 @@ def create_publication_files(root_dir):
         filepath = pub_dir / filename
         citation = f"{pub['authors']} ({pub['date'][:4]}) \"{pub['title']}.\" <i>{pub['venue']}</i>."
         
-        # *** THIS IS THE FIX: The 'collection: publications' line is ADDED BACK ***
+        # *** THIS IS THE CORRECTED CODE BLOCK ***
+        # The 'collection: publications' line is now correctly included.
         content = f"""---
 title: "{pub['title']}"
 collection: publications
